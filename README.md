b682146543c0229525a9d6dc9b91e252847b03c2
josefandino <websysfan@gmail.com>
2021-12-16 10:51:29 -0500

    SQLite - Cordova

---
+++ README.md
@@ -0,0 +1,2 @@
+# ionic-sqlite-cordova
+
---
+++ src/app/create/create.page.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+import { CrudService } from '../crud.service';
+
+@Component({
+  selector: 'app-create',
+  templateUrl: './create.page.html',
+  styleUrls: ['./create.page.scss'],
+})
+
+export class CreatePage implements OnInit {
+
+  nameVal: string = "";
+  emailVal: string = "";
+
+  constructor(
+    private crud: CrudService
+  ) {
+    this.crud.databaseConn(); 
+  }
+
+  ngOnInit() { }
+
+  ionViewDidEnter() {  
+    this.crud.getAllUsers()
+  }
+
+  createUser(){
+    this.crud.addItem(this.nameVal, this.emailVal);
+    console.log('Add info');
+    
+  }
+
+  remove(user) {
+    this.crud.deleteUser(user);
+  }
+  
+}
